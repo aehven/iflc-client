9904de4f580bfda41692476fa9dc36f02fb9e692
Adam Ehven <aehven@binarytrees.biz>
2017-07-22 09:26:54 -0600

    tweaks

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiPath: "http://silversurfer.local:3000",
+  apiPath: "http://192.168.2.10:3000",
   demoUser: "a2@null.com",
   demoPassword: "password"
 };
