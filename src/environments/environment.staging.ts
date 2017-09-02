dd7b3dac35ea85201ddfd7e8402b182758aa7289
Adam Ehven <aehven@binarytrees.biz>
2017-09-02 13:34:09 -0600

    oops

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiPath: "https://iflc-production.herokuapp.com",
+  apiPath: "https://iflc-staging.herokuapp.com",
   demoUser: "a2@null.com",
   demoPassword: "password"
 };
